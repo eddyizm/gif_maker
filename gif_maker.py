6f551db47102ebb455554398196226a0230f3045
eddyizm <eddyizm@gmail.com>
2023-02-07 18:32:34 -0800

    gif created successfully

--- gif_maker.py
+++ gif_maker.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+from datetime import datetime
 from PIL import Image
 from rich import print
 from rich.progress import track
@@ -29,10 +30,23 @@ def scale_image(input_image_path,
 
 
 def resize_images(image_path, glob_regex):
+    print('Resizing images...')
     search = os.path.join(image_path, glob_regex)
     for image in track(glob.glob(search)):
         out = image + '.resized.png'
         scale_image(image, out, width=800)
+    print('resizing done')
+
+def make_gif(image_path, glob_regex,  gif_name = None):
+    print('making gif!')
+    gif_name = gif_name or f'{datetime.now().strftime("%Y%m%d%H%M")}_gif_maker.gif'
+    search = os.path.join(image_path, glob_regex)
+    outfile = os.path.join(image_path, gif_name)
+    frames = [Image.open(image) for image in glob.glob(search)]
+    frame_one = frames[0]
+    frame_one.save(outfile, format="GIF", append_images=frames,
+               save_all=True, duration=100, loop=0)
+    print(f'gif created: {gif_name}')
 
 
 def main():
@@ -45,6 +59,7 @@ def main():
         return
     fp_in = sys.argv[1]
     resize_images(fp_in, 'vlcsnap*.png')
+    make_gif(fp_in, 'vlcsnap*.resized.png')
     
 
 
