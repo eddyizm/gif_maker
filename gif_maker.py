0267ac60ecff528c886e320535acb21c0f368db0
eddyizm <eddyizm@gmail.com>
2023-02-07 17:48:07 -0800

    started base file

---
+++ gif_maker.py
@@ -0,0 +1,15 @@
+import glob
+import sys
+from PIL import Image
+
+  
+def main():
+    print(sys.argv)
+    # TODO flag to clean up original files
+    # TODO flag to scale down
+    # TODO flag to specify scale down size
+
+
+if __name__ == "__main__":
+    main()    
+    # make_gif()
